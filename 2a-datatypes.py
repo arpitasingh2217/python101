c5bc2040b327f7836c3587e8b3f992a5f688aab7
Arpita Singh <arpitasingh2217@gmail.com>
2024-04-02 17:15:02 +0000

    DATATYPE

---
+++ 2a-datatypes.py
@@ -0,0 +1,10 @@
+#startswith
+string = "Arpita Singh "
+print(string.startswith("A")) # Boolean True
+print(string.endswith("H")) # Boolean True
+print(string.find("I")) # int 4
+print(string.index("A")) # int 4
+print(string.startswith("e")) # Boolean False
+print(string.endswith("H")) # Boolean False
+print(string)
+print(string.endswith(" ")) # Boolean True
